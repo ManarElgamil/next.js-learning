17add8ac63373741615ee00167d9817c19886e49
Manar Gamil <manargamil@Manars-MacBook-Air.local>
2025-12-22 13:08:12 -0400

    Convert first-basic-app from submodule to normal folder

---
+++ section1/first-basic-app/src/app/scale/page.tsx
@@ -0,0 +1,8 @@
+// This function is basic React component that represents the Scale page.
+// without it there is no react components, 
+// so if you visit the route without exporting the function it will throw an error
+export default function ScalePage() {
+    return (
+        <div>Scale Page</div>
+    );
+}
